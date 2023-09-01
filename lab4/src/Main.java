f1b250b79a9582d577f8f1b5ebc769a0b4b91262
sudipto0315 <sudiptoray2003@gmail.com>
2023-09-02 00:02:40 +0530

    lab 4

--- lab4/src/Main.java
+++ lab4/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Lab 4");
+        System.out.println("Credits to Sreejita Das for generously " +
+                "lending me the framework for this Music Management System");
+
     }
 }
